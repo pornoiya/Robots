9de7d20da5261e8c637f87ba7d1f6609731125cf
pornoiya <49945883+pornoiya@users.noreply.github.com>
2020-05-18 00:09:41 +0500

    task2 refactored

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -1,18 +1,20 @@
 package gui;
 
-import java.awt.*;
-import java.awt.desktop.SystemEventListener;
-import java.awt.event.*;
-import java.util.Locale;
+import log.Logger;
 
 import javax.swing.*;
-
-import log.Logger;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 /**
  * Что требуется сделать:
- * 1. Метод создания меню перегружен функционалом и трудно читается. 
+ * 1. Метод создания меню перегружен функционалом и трудно читается.
  * Следует разделить его на серию более простых методов (или вообще выделить отдельный класс).
  *
  */
@@ -32,6 +34,12 @@ public class MainApplicationFrame extends JFrame{
         setContentPane(desktopPane);
 
 
+        var jsonStatesFile = new File("windowStates.json");
+        var states = new HashMap<String, HashMap>();
+        if (jsonStatesFile.exists() && !jsonStatesFile.isDirectory()) {
+            states = WindowStateControl.readState(jsonStatesFile);
+        }
+
         LogWindow logWindow = createLogWindow();
         addWindow(logWindow);
 
@@ -39,13 +47,25 @@ public class MainApplicationFrame extends JFrame{
         gameWindow.setSize(400,  400);
         addWindow(gameWindow);
 
-        addWindowListener(createExitActionListener());
+        var windows = new ArrayList<JInternalFrame>();
+        windows.add(gameWindow);
+        windows.add(logWindow);
+        addWindowListener(createExitActionListener(windows));
+
+        if(states != null){
+            for (JInternalFrame w: windows){
+                if (states.containsKey(w.getTitle())){
+                    var currentWindowState = states.get(w.getTitle());
+                    WindowStateControl.resetStates(w, currentWindowState);
+                }
+            }
+        }
 
         setJMenuBar(generateMenuBar());
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
     }
 
-    protected WindowAdapter createExitActionListener() {
+    protected WindowAdapter createExitActionListener(ArrayList<JInternalFrame> frames) {
         return new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -57,6 +77,8 @@ public class MainApplicationFrame extends JFrame{
                         "Окно подтверждения",
                         JOptionPane.YES_NO_OPTION);
                 if (result == JOptionPane.YES_OPTION){
+                    var states = WindowStateControl.getState(frames);
+                    WindowStateControl.saveState(states);
                     System.exit(0);
                 }
             }
@@ -82,12 +104,12 @@ public class MainApplicationFrame extends JFrame{
 
 //    protected JMenuBar createMenuBar() {
 //        JMenuBar menuBar = new JMenuBar();
-// 
+//
 //        //Set up the lone menu.
 //        JMenu menu = new JMenu("Document");
 //        menu.setMnemonic(KeyEvent.VK_D);
 //        menuBar.add(menu);
-// 
+//
 //        //Set up the first menu item.
 //        JMenuItem menuItem = new JMenuItem("New");
 //        menuItem.setMnemonic(KeyEvent.VK_N);
@@ -96,7 +118,7 @@ public class MainApplicationFrame extends JFrame{
 //        menuItem.setActionCommand("new");
 ////        menuItem.addActionListener(this);
 //        menu.add(menuItem);
-// 
+//
 //        //Set up the second menu item.
 //        menuItem = new JMenuItem("Quit");
 //        menuItem.setMnemonic(KeyEvent.VK_Q);
@@ -105,7 +127,7 @@ public class MainApplicationFrame extends JFrame{
 //        menuItem.setActionCommand("quit");
 ////        menuItem.addActionListener(this);
 //        menu.add(menuItem);
-// 
+//
 //        return menuBar;
 //    }
 
